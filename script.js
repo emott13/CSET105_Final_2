7bc9c0fd16bb1af57267fe94c12dd094d00b88cf
mariely <lorenzomariely@gmail.com>
2024-10-22 10:52:31 -0400

    10 questions

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-//syntax for questions
-// const questions = [{
-//     name: "question here", answer: "answer here", 
-//     options: [{title: 'option1'}, {title: 'option2'}, {title: 'option3'}, {title: 'option4'}]
-// }];
+
+const questions = [{
+    name: "Where is the Eiffel Tower?", answer: "2", 
+    options: [{title: 'Italy'}, {title: 'Russia'}, {title: 'Paris'}, {title: 'Canada'}]
+},
+ {name: "What is the capital of the U.S.A?", answer: "2", 
+    options: [{title: 'L.A'}, {title: 'Harrisburg'}, {title: 'Washington D.C'}, {title: 'Texas'}]
+},
+ {name: "which of these is an animal?", answer: "3", 
+    options: [{title: 'Marker'}, {title: 'Computer'}, {title: 'Lion'}, {title: 'Desk'}]
+},
+{name: "What color is the sky?", answer: "0", 
+    options: [{title: 'Blue'}, {title: 'Red'}, {title: 'Pink'}, {title: 'Apple'}]
+},
+{name: "Where is chinese the official language in?", answer: "1", 
+    options: [{title: 'Japan'}, {title: 'China'}, {title: 'America'}, {title: 'Russia'}]
+},
+{name: "Who was the first president of the U.S?", answer: "0", 
+    options: [{title: 'George Washington'}, {title: 'George Bush'}, {title: 'Donald Trump'}, {title: 'Joe Biden'}]
+},
+{name: "What phone brand has a bitten fruit logo?", answer: "1", 
+    options: [{title: 'HP'}, {title: 'Apple'}, {title: 'Galaxy'}, {title: 'Google pixel'}]
+},
+{name: "What is the name of your CSET teacher", answer: "3", 
+    options: [{title: 'Mister'}, {title: 'Mr. Stevens'}, {title: 'Mr. Christopher'}, {title: 'Mr. Chowdhury'}]
+},
+{name: "What animal is considered a humans best friend?", answer: "0", 
+    options: [{title: 'Dog'}, {title: 'Gorilla'}, {title: 'Hippo'}, {title: 'Mermaid'}]
+},
+{name: "What is the name of your College?", answer: "2", 
+    options: [{title: 'Texas University'}, {title: 'Penn State College'}, {title: 'Thaddeus Stevens College of Technology'}, {title: 'Harvard University'}]
+},
+    ];
